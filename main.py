2050b837ae63c275a5e796780953479b219931e9
bit-to-bit <vasiliy.pykhteev@gmail.com>
2025-06-26 01:31:29 +0300

    remove_print_hello

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from src.database.seeds import generate_data
 from src.database.my_select import execute_all_selects
 
 if __name__ == "__main__":
-    print("Hello")
     generate_data()
     execute_all_selects()
